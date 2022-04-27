81974dc3508cab302b35b2482512d7762cbe8255
Cyril Le Goas <cyril.legoas@gmail.com>
2022-04-27 16:55:51 +0200

    ADD message if no recipe found

--- src/js/cards.js
+++ src/js/cards.js
@@ -4,45 +4,49 @@ export const recipesCards = {
   cardsContainer: document.querySelector('.recipes'),
   addCards(ids) {
     let html = ``;
-    ids.forEach((id) => {
-      html += `
-        <li class="recipe">
-          <img class="recipe__img" src="https://picsum.photos/380/300" alt="" />
-          <div class="recipe__desc">
-            <div class="recipe__title-wrapper">
-              <h2 class="recipe__title">${allRecipes.table.get(id).name}</h2>
-              <div class="recipe__timer-wrapper">
-                <img
-                  class="recipe__timer-icon"
-                  src="./public/icons/clock.svg"
-                  alt="clock"
-                />
-                <span class="recipe__timer-min">${
-                  allRecipes.table.get(id).time
-                } min</span>
+    if (!ids.length)
+      html = `<li class="no-recipes">Aucune recette ne correspond à votre critère, vous pouvez chercher « tarte aux pommes », « poisson », etc...</li>`;
+    else {
+      ids.forEach((id) => {
+        html += `
+          <li class="recipe">
+            <img class="recipe__img" src="https://picsum.photos/380/300" alt="" />
+            <div class="recipe__desc">
+              <div class="recipe__title-wrapper">
+                <h2 class="recipe__title">${allRecipes.table.get(id).name}</h2>
+                <div class="recipe__timer-wrapper">
+                  <img
+                    class="recipe__timer-icon"
+                    src="./public/icons/clock.svg"
+                    alt="clock"
+                  />
+                  <span class="recipe__timer-min">${
+                    allRecipes.table.get(id).time
+                  } min</span>
+                </div>
+              </div>
+              <div class="recipe__instructions-wrapper">
+                <ul class="recipe__ingredients">
+                  ${(() =>
+                    allRecipes.table
+                      .get(id)
+                      .ingredients.reduce((itemList, item) => {
+                        itemList += `<li class="recipe__ingredient"><span>${
+                          item.ingredient
+                        }:</span> ${item.quantity} ${
+                          item.unit ? item.unit : ''
+                        }</li>`;
+                        return itemList;
+                      }, ''))()}
+                </ul>
+                <p class="recipe__instructions">${
+                  allRecipes.table.get(id).description
+                }</p>
               </div>
             </div>
-            <div class="recipe__instructions-wrapper">
-              <ul class="recipe__ingredients">
-                ${(() =>
-                  allRecipes.table
-                    .get(id)
-                    .ingredients.reduce((itemList, item) => {
-                      itemList += `<li class="recipe__ingredient"><span>${
-                        item.ingredient
-                      }:</span> ${item.quantity} ${
-                        item.unit ? item.unit : ''
-                      }</li>`;
-                      return itemList;
-                    }, ''))()}
-              </ul>
-              <p class="recipe__instructions">${
-                allRecipes.table.get(id).description
-              }</p>
-            </div>
-          </div>
-        </li>`;
-    });
+          </li>`;
+      });
+    }
     this.cardsContainer.innerHTML = html;
   },
   clearCards() {
